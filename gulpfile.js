385bdeff11a6156b583f6522b1d5c4eb29b7d224
Arun Tiwari <arunstiwari@gmail.com>
2015-02-23 22:42:24 +0530

    Added build script

--- gulpfile.js
+++ gulpfile.js
@@ -11,52 +11,120 @@ var gulp = require('gulp'),
 	minifyCss = require('gulp-minify-css'),
 	inject = require('gulp-inject'),
 	path = require('path'),
-	filter = require('gulp-filter');
+	filter = require('gulp-filter'),
+	rev = require('gulp-rev'),
+	revCollector = require('gulp-rev-collector'),
+	minifyHTML = require('gulp-minify-html'),
+	rimrafD = require('rimraf'),
+	rimraf = require('gulp-rimraf'),
+	igonre = require('gulp-ignore'),
+	uglify = require('gulp-uglify');
 
-gulp.task('vendorcss', function(){
-	return gulp
-	.src(config.paths.vendorcss)
-	.pipe(concat('vendor.min.css'))
-	.pipe(bytediff.start())
-	.pipe(minifyCss())
-	.pipe(bytediff.stop(bytediffFormatter))
-	.pipe(gulp.dest(config.paths.destination));
-});
-
-gulp.task('rev-and-inject',['vendorcss'],function(){
-	var indexPath = path.join(config.paths.source, 'index.html');
+	gulp.task('cleandist',function(cb){
+		rimrafD('./dist',cb);
+	})
 	
-	var localInject = function(pathGlob, name) {
-		var options = {
-		ignorePath: config.paths.destination,
-		read: false,
-		addPrefix: config.paths.buildPrefix
-		};
-		if (name) {
-		options.name = name;
-		}
+	gulp.task('cleanrev',function(cb){
+		rimrafD('./rev',cb);
+	})
+	
+	gulp.task('cleanminify',function(cb){
+		rimrafD('./minify',cb);
+	})
+	
+	gulp.task('createVendorCss',function(){
+		return gulp.src('src/main/resources/static/js/bower_components/bootstrap/dist/css/bootstrap.css')
+					.pipe(concat('bootstrap.css'))
+					.pipe(minifyCss())
+					.pipe(gulp.dest('dist/css'))
+					.pipe(rev())
+					.pipe(gulp.dest('rev/css/vendor'))
+					.pipe(rev.manifest())
+					.pipe(gulp.dest('rev/css/vendor'));
+	});
+	
+	gulp.task('createAppCss',function(){
+		return gulp.src('src/main/resources/static/css/app.css')
+					.pipe(concat('app.css'))
+					.pipe(minifyCss())
+					.pipe(gulp.dest('dist/css'))
+					.pipe(rev())
+					.pipe(gulp.dest('rev/css/app'))
+					.pipe(rev.manifest())
+					.pipe(gulp.dest('rev/css/app'));
+	});
+	
+	gulp.task('createVendorJs',function(){
 		
-		return inject(gulp.src(pathGlob),options);
-	};
+		return gulp.src(config.vendorjs)
+					.pipe(concat('vendor.min.js'))
+					.pipe(uglify())
+					.pipe(gulp.dest('dist/js'))
+					.pipe(rev())
+					.pipe(gulp.dest('rev/js/vendor'))
+					.pipe(rev.manifest())
+					.pipe(gulp.dest('rev/js/vendor'));
+	});
 	
-	return gulp
-			.src([].concat(indexPath))
-			.pipe(localInject(path.join(config.paths.destination,'vendor.min.css'),'inject-vendor'))
-			.pipe(gulp.dest(config.paths.distribution))
-});
+gulp.task('createAppJs',function(){
+		
+		return gulp.src(config.appjs)
+					.pipe(concat('site.min.js'))
+					.pipe(uglify())
+					.pipe(gulp.dest('dist/js'))
+					.pipe(rev())
+					.pipe(gulp.dest('rev/js/app'))
+					.pipe(rev.manifest())
+					.pipe(gulp.dest('rev/js/app'));
+	});
 
-gulp.task('default',function(){
-	gulp.start('rev-and-inject');
-});
+	gulp.task('css',function(){
+		return gulp.src('src/main/resources/static/css/*.css')
+					.pipe(rev())
+					.pipe(gulp.dest('dist/css'))
+					.pipe(rev.manifest())
+					.pipe(gulp.dest('rev/css'));
+	});
+	
+	gulp.task('scripts', function(){
+		return gulp.src('src/main/resources/static/js/**/*.js')
+					.pipe(rev())
+					.pipe(gulp.dest('dist/js'))
+					.pipe(rev.manifest())
+					.pipe(gulp.dest('rev/js'));
+	});
+	
+	gulp.task('rev1',['css','scripts'],function(){
+		return gulp.src(['rev/**/*.json','src/main/resources/templates/**/*.html'])
+					.pipe(revCollector({
+						replaceReved: true,
+						dirReplacements: {
+							'css':'/dist/css',
+							'js': '/dist/js/',
+							'bootstrap.css': '/dist/css'
+						}
+					}))
+					/*.pipe(minifyHTML({
+						empty: true,
+						spare: true
+					}))*/
+					.pipe(gulp.dest('rev'));
+	})
 
-function bytediffFormatter(data) {
-	var formatPercent = function(num, precision) {
-		return (num * 100).toFixed(precision);
-	};
-    var difference = (data.savings > 0) ? ' smaller.' : ' larger.';
-    
-    return data.fileName + ' went from ' +
-        (data.startSize / 1000).toFixed(2) + ' kB to ' + (data.endSize / 1000).toFixed(2) + ' kB' +
-        ' and is ' + formatPercent(1 - data.percent, 2) + '%' + difference;
-}
+	
+	gulp.task('rev',['createVendorCss','createAppCss','createVendorJs','createAppJs'],function(){
+		return gulp.src(['rev/**/*.json','src/main/resources/templates/**/*.html'])
+					.pipe(revCollector({
+						replaceReved: true,
+						dirReplacements: {
+							'css':'dist/css',
+							'js': 'dist/js'
+						}
+					}))
+					/*.pipe(minifyHTML({
+						empty: true,
+						spare: true
+					}))*/
+					.pipe(gulp.dest('rev'));
+	})
 
